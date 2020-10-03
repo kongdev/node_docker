d56fde92927dd79166fbea25102a6b6afa68be7d
kongdev <nopaket_kong@hotmail.com>
2020-10-04 03:40:25 +0700

    add pm2

--- app1/index.js
+++ app1/index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var app = express();
-
+var logger = require("morgan");
 var port = 1111;
 
+app.use(logger('dev'))
 app.get("/", (req, res) => {
   res.send('app 1 port 1111');
 });
