2217547510c7eb33db2f0d2f46ea8d12614c9cd2
kongdev <nopaket_kong@hotmail.com>
2024-12-17 14:35:59 +0700

    poc log

--- app1/index.js
+++ app1/index.js
@@ -1,10 +1,43 @@
 var express = require("express");
+const correlator = require('express-correlation-id');
+
+
+
+
+const axios = require('axios');
+
 var app = express();
 var logger = require("morgan");
 var port = 1111;
 
 app.use(logger('dev'))
+app.use(correlator());
+
+
+
+const aa = () => {
+  console.log('func aa request is:', correlator.getId()); // equal to above, not dependant on the req object
+}
+const bb = () => {
+  console.log('func bb request is:', correlator.getId()); // equal to above, not dependant on the req object
+  cc();
+}
+
+const cc = () => {
+  console.log('func cc request is:', correlator.getId()); // equal to above, not dependant on the req object
+}
+
+
+
 app.get("/", (req, res) => {
+  console.log('endpoint get request express is:', req.correlationId()); // id for this request
+  console.log('endpoint get request correlator is:', correlator.getId()); // equal to above, not dependant on the req object
+  
+  aa();
+  bb();
+  // axios.get('http://localhost:2222',{ headers: { 'X-Correlation-Id': correlator.getId() } }).then(resp => { });
+
+  
   res.send('app 1 port 1111');
 });
 
--- app2/index.js
+++ app2/index.js
@@ -1,9 +1,14 @@
 var express = require("express");
+const correlator = require('express-correlation-id');
 var app = express();
-
+var logger = require("morgan");
 var port = 2222;
 
+app.use(logger('dev'))
+app.use(correlator());
 app.get("/", (req, res) => {
+  console.log('ID for this request is:', req.correlationId()); // id for this request
+  console.log('ID for this request is:', correlator.getId()); // equal to above, not dependant on the req object
   res.send('app 2 port 2222');
 });
 
